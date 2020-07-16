083d31dbc333003b6bb280644fb82e25eb764ba7
Robert-Rodriguez <robertmasters@hotmail.com>
2020-07-16 14:48:37 -0700

    on task 3

--- index.js
+++ index.js
@@ -7,12 +7,26 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
 function createMenuItem(name, cost, category){
-    /* Code here */
+    obj = {name, cost, category};
+    return obj;
 }
+const steak = createMenuItem("Steak",35, "Dinner")
+console.log(steak);
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
+const taco = createMenuItem("Taco", 3, "Lunch")
+console.log(taco);
+const sushi = createMenuItem("Sushi", 45, "Dinner")
+console.log(sushi);
+const eggs = createMenuItem("Eggs", 10, "Breakfast")
+console.log(eggs);
+
+console.log('----------');
 
 
+console.log(burger.price);
+
+console.log('----------');
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
 
@@ -23,6 +37,24 @@ Your method should accept:
 and should return a number. 
 
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
+
+burger.autoCalc = function(discount){
+  let rate = 0;
+
+  //if else to determine who the discount will be for and based on that who, what percentage(used decimal for calculations) of discount will be given
+  if(discount==="teacher" || discount === "student"){
+    rate = .25;
+  } else {
+    rate = .1
+  }
+  let sum = this.price - (rate*this.price); // .this is used to reffer to the key price in the burger object.
+  return sum;
+}
+
+console.log(burger.autoCalc('teacher')); // test
+console.log(burger.autoCalc('student')); // test
+console.log(burger.autoCalc('public')); //test
+console.log(burger.autoCalc('random person')); //test
 
 
 
@@ -40,6 +72,9 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 
 /* Task 3: Console.log just Julius' feedback */
 
+
+
+console.log(object.values);
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
 
