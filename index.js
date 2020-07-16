f087915d6b9b74eba5706ecc751127e7714aac42
Robert-Rodriguez <robertmasters@hotmail.com>
2020-07-16 14:56:00 -0700

    finished task 3

--- index.js
+++ index.js
@@ -72,11 +72,10 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 
 /* Task 3: Console.log just Julius' feedback */
 
-
-
-console.log(object.values);
+console.log(reviews[5].feedback);
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
+
 
 
 /* Task 5: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
